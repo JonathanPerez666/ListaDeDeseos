0fa0790891e852b2771406a4527fce31069875aa
Jonathan <mcjhona069@gmail.com>
2020-11-26 22:04:54 -0600

    Pipes impuros

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListasComponent } from './listas/listas.component';
 import { IonicModule } from '@ionic/angular';
-
+import { PipesModule } from '../pipes/pipes.module';
 
 
 @NgModule({
@@ -14,7 +14,8 @@ import { IonicModule } from '@ionic/angular';
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    PipesModule
   ]
 })
 export class ComponentsModule { }
